40fcb446149e76257b0edcbd63c9e1f321c8849c
Jean Carlos Nesi <jean.carlos_nesi@hotmail.com>
2024-05-31 12:12:09 -0300

    .

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,2 @@
 /* eslint-disable no-console */
 import 'dotenv/config';
-import { server } from './config/server';
-
-server.listen(process.env.PORT || 8080, () =>
-  console.log('\n\n\n 🚀️ Server is running 🚀️ \n\n\n'),
-);
