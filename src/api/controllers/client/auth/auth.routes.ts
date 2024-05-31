18a8a5bc4245624015516f715aa2e5c0e33b8c37
Jean Carlos Nesi <jean.carlos_nesi@hotmail.com>
2024-05-31 11:18:26 -0300

    .

--- src/api/controllers/client/auth/auth.routes.ts
+++ src/api/controllers/client/auth/auth.routes.ts
@@ -6,3 +6,4 @@ export const authRouter = Router();
 
 authRouter.post('/login', clientLoginController);
 authRouter.post('/register', clientRegisterController);
+authRouter.get('/test', (_req, res) => res.status(200).send('OK'));
